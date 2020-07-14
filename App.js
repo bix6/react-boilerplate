556211a8ac34bf532b5d6fdb847569530a7348cb
bix <840573+bix6@users.noreply.github.com>
2020-07-13 18:59:25 -0700

    add config.js, example.env. add WebSockets. format

--- App.js
+++ App.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import LandingPage from './LandingPage/LandingPage';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import LandingPage from "./LandingPage/LandingPage";
+
+// WebSocket Setup
+/*
+import io from "socket.io-client";
+import config from "./config";
+const socket = io.connect(config.API_ENDPOINT);
+*/
 
 class App extends React.Component {
-    render() {
-        return (
-            <>
-                <h1>App</h1>
-                <Switch>
-                    <Route exact path='/' component={ LandingPage } />
-                </Switch>
-            </>
-        );
-    }
+  render() {
+    return (
+      <>
+        <h1>App</h1>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+        </Switch>
+      </>
+    );
+  }
 }
 
 export default App;
--- LandingPage/LandingPage.test.js
+++ LandingPage/LandingPage.test.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { shallow } from 'enzyme';
-import toJson from 'enzyme-to-json';
-import { BrowserRouter } from 'react-router-dom';
-import LandingPage from './LandingPage';
+import React from "react";
+import ReactDOM from "react-dom";
+import { shallow } from "enzyme";
+import toJson from "enzyme-to-json";
+import { BrowserRouter } from "react-router-dom";
+import LandingPage from "./LandingPage";
 
-describe('LandingPage Component', () => {
-    it('Smoke Test: Renders Empty', () => {
-        const div = document.createElement('div');
-        ReactDOM.render(
-            <BrowserRouter>
-                <LandingPage />
-            </BrowserRouter>,
-            div);
-        ReactDOM.unmountComponentAtNode(div);
-    })
+describe("LandingPage Component", () => {
+  it("Smoke Test: Renders Empty", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(
+      <BrowserRouter>
+        <LandingPage />
+      </BrowserRouter>,
+      div
+    );
+    ReactDOM.unmountComponentAtNode(div);
+  });
 
-    it('Snapshot Test: Empty', () => {
-        const wrapper = shallow(<LandingPage />);
-        expect(toJson(wrapper)).toMatchSnapshot();
-    })
-})
+  it("Snapshot Test: Empty", () => {
+    const wrapper = shallow(<LandingPage />);
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+});
